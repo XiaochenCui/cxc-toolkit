e56e261e986cd0f01cabf716ffe19b91726fd039
XiaochenCui <jcnlcxc.new@gmail.com>
2018-01-17 16:32:20 +0800

    Add test for string_to_hex

--- test/test_hodgepodge.py
+++ test/test_hodgepodge.py
@@ -1,6 +1,6 @@
 import unittest
 
-from cxc_toolkit.hodgepodge import xor
+from cxc_toolkit.hodgepodge import xor, string_to_hex
 
 
 class TestHodgepodge(unittest.TestCase):
@@ -13,3 +13,14 @@ class TestHodgepodge(unittest.TestCase):
         xor_hex = "d2dd6bc9b9c334f"
         assert xor_hex == xor(hex_1, hex_2)
         assert xor_hex == xor(hex_2, hex_1)
+
+    def test_string_to_hex(self):
+        string = "123"
+        hex_string = "313233"
+        assert string_to_hex(string) == hex_string
+        string = "az9?"
+        hex_string = "617a393f"
+        assert string_to_hex(string) == hex_string
+        string = "崔晓晨"
+        hex_string = "e5b494e69993e699a8"
+        assert string_to_hex(string) == hex_string
